cb71f2c9afa6327d50a98bc7b6455db588dba460
Jabba Laci <jabba.laci@gmail.com>
2021-12-06 07:31:05 +0100

    Day 6: Lanternfish

---
+++ day06/python/part1.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+from collections import Counter
+
+import helper
+
+
+def my_print(d: dict[int, int], msg: str = "") -> None:
+    sb = []
+    for key in range(0, 8+1):
+        sb.append(f"{key}: {d[key]}")
+    #
+    print(f"{msg}: ", end="")
+    print('{' + ", ".join(sb) + '}', flush=True)
+
+
+def main():
+    # fname = "example.txt"
+    fname = "input.txt"
+
+    numbers = [int(s) for s in helper.read(fname).split(",")]
+    d = dict(Counter(numbers))
+    for key in range(0, 8+1):
+        if key not in d:
+            d[key] = 0
+        #
+    #
+
+    for k in range(80):
+        my_print(d, msg=f"{k}")
+
+        cnt = 0
+        for key in range(0, 8+1):
+            if key == 0:
+                cnt = d[key]
+            else:
+                d[key-1] = d[key]
+                d[key] = 0
+            #
+        #
+        d[6] += cnt
+        d[8] = cnt
+
+        # if k == 6:
+            # break
+        #
+    #
+    print("---")
+    result = sum(d.values())
+    print(result)
+
+##############################################################################
+
+if __name__ == "__main__":
+    main()
---
+++ day06/python/slow/part2.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import helper
+
+
+def main():
+    fname = "example.txt"
+    # fname = "input.txt"
+    numbers = [int(s) for s in helper.read(fname).split(",")]
+    # print(numbers)
+
+    for k in range(256):
+        print(f"{k+1}: {len(numbers)}", flush=True)
+        cnt = 0
+        for i in range(len(numbers)):
+            value = numbers[i]
+            value -= 1
+            if value == -1:
+                value = 6
+                cnt += 1
+            #
+            numbers[i] = value
+        #
+        for i in range(cnt):
+            numbers.append(8)
+        #
+        # print(numbers)
+
+        # if k == 17:
+            # break
+        #
+    #
+    print("---")
+    result = len(numbers)
+    print(result)
+
+##############################################################################
+
+if __name__ == "__main__":
+    main()
